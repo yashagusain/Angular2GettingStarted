5c04cdfd7123626fcf5815071e49bb8de09cccf4
yasha <yasha@yasha-Vostro-3445.(none)>
2017-08-02 10:55:08 +1000

    dependency injection

--- APM/app/app.module.js
+++ APM/app/app.module.js
@@ -9,8 +9,12 @@ Object.defineProperty(exports, "__esModule", { value: true });
 var core_1 = require("@angular/core");
 var platform_browser_1 = require("@angular/platform-browser");
 var forms_1 = require("@angular/forms");
+var http_1 = require("@angular/http");
+var router_1 = require("@angular/router");
 var app_component_1 = require("./app.component");
+var welcome_component_1 = require("./home/welcome.component");
 var product_list_component_1 = require("./products/product-list.component");
+var product_detail_component_1 = require("./products/product-detail.component");
 var product_filter_pipe_1 = require("./products/product-filter.pipe");
 var star_component_1 = require("./shared/star.component");
 var AppModule = (function () {
@@ -21,10 +25,22 @@ var AppModule = (function () {
 AppModule = __decorate([
     core_1.NgModule({
         imports: [platform_browser_1.BrowserModule,
-            forms_1.FormsModule],
-        declarations: [app_component_1.AppComponent,
+            forms_1.FormsModule,
+            http_1.HttpModule,
+            router_1.RouterModule.forRoot([
+                { path: 'products', component: product_list_component_1.ProductListComponent },
+                { path: 'products/:id', component: product_detail_component_1.ProductDetailComponent },
+                { path: 'welcome', component: welcome_component_1.WelcomeComponent },
+                { path: '', redirectTo: 'welcome', pathMatch: 'full' },
+                { path: '**', redirectTo: 'welcome', pathMatch: 'full' }
+            ])
+        ],
+        declarations: [
+            app_component_1.AppComponent,
             product_list_component_1.ProductListComponent,
-            product_filter_pipe_1.ProductFilterPipe, star_component_1.StarComponent],
+            product_filter_pipe_1.ProductFilterPipe, star_component_1.StarComponent,
+            product_detail_component_1.ProductDetailComponent, welcome_component_1.WelcomeComponent
+        ],
         bootstrap: [app_component_1.AppComponent]
     })
 ], AppModule);
--- APM/app/app.module.ts
+++ APM/app/app.module.ts
@@ -1,16 +1,32 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule }  from '@angular/forms';
+import {HttpModule} from '@angular/http';
+import {RouterModule} from '@angular/router';
+
 import { AppComponent }  from './app.component';
+import {WelcomeComponent} from './home/welcome.component';
 import { ProductListComponent } from "./products/product-list.component";
+import {ProductDetailComponent} from "./products/product-detail.component";
 import {ProductFilterPipe} from "./products/product-filter.pipe";
 import {StarComponent} from "./shared/star.component"
 @NgModule({
   imports: [ BrowserModule,
-  FormsModule ],
-  declarations: [ AppComponent ,
-  ProductListComponent,
-  ProductFilterPipe,StarComponent],
+  FormsModule,
+  HttpModule,
+  RouterModule.forRoot([
+    {path:'products',component:ProductListComponent},
+    {path:'products/:id',component:ProductDetailComponent},
+  {path:'welcome',component:WelcomeComponent},
+  {path:'',redirectTo:'welcome',pathMatch:'full'},
+  {path:'**',redirectTo:'welcome',pathMatch:'full'}
+  ])
+  ],
+    declarations: [
+       AppComponent ,
+       ProductListComponent,
+  ProductFilterPipe,StarComponent,
+  ProductDetailComponent,WelcomeComponent],
   bootstrap: [ AppComponent ]
 })
 export class AppModule { }
